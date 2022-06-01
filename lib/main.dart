eec4229bfbb0f7c68fd02ae5ac78f578dd7424bc
bourigue <rrbourigue@gmail.com>
2022-06-01 12:12:18 +0100

    fix: fixed text

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'i m rachid :',
             ),
             Text(
               '$_counter',
