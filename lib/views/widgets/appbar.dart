aee8273daed3b60b2266b8114c6dd12f155ebdc5
Ray0Emma <ahmadifarah22@gmail.com>
2022-06-02 23:19:39 +0000

    feat: added searchbar widget

--- lib/views/widgets/appbar.dart
+++ lib/views/widgets/appbar.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import '../widgets/searchBar.dart';
 
 class MyAppBar extends StatefulWidget {
   const MyAppBar({Key? key}) : super(key: key);
@@ -33,36 +34,8 @@ class _MyAppBarState extends State<MyAppBar> {
             top: 80.0,
             left: 0.0,
             right: 0.0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Color(0xffF5F7F9)),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Search by city",
-                          contentPadding: EdgeInsets.all(20.0),
-                        ),
-                        keyboardType: TextInputType.text,
-                        maxLines: 1,
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.search,
-                        color: Color(0xff7C7B90),
-                      ),
-                      onPressed: null,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            // **** search bar ****
+            child: SearchBar(),
           ),
         ],
       ),
---
+++ lib/views/widgets/searchbar.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), color: Color(0xffF5F7F9)),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                cursorColor: Color(0xff4943F0),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search by city",
+                  contentPadding: EdgeInsets.all(20.0),
+                ),
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Color(0xff7C7B90),
+              ),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
