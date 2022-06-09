0d145a77f6ace9d4ad5f316bdb3706d47cd733f0
bourigue <rrbourigue@gmail.com>
2022-06-09 10:00:00 +0100

    fix>add screnn register

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 // ignore_for_file: prefer_const_constructors, avoid_print, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:roomie/views/resources/app_colors.dart';
 import 'package:roomie/views/splach_screen.dart';
-
+import 'package:get/get.dart';
+import 'package:roomie/views/login.dart';
+import 'package:flutter/services.dart';
 void main() {
+  theme: ThemeData(
+    iconTheme: const IconThemeData(color:Colors.white),
+    inputDecorationTheme: InputDecorationTheme(focusColor: Colors.white),
+      colorScheme: ThemeData().colorScheme.copyWith(
+        secondary: Colors.red)
+  );
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: AppColors.PRIMARY_COLOR));
   runApp(const MyApp());
 }
 
@@ -13,9 +23,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Roomie',
+      color: AppColors.PRIMARY_COLOR,
       theme: ThemeData(
+
         primarySwatch: Colors.blue,
       ),
       home: SplashScreen(),
