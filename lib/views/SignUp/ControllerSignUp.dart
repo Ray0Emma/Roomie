901aa2cdf30ca37fb2c46b98c88f2bfb0d50b003
bourigue <rrbourigue@gmail.com>
2022-06-11 23:56:08 +0100

    fead :add conterollersigne

---
+++ lib/views/SignUp/ControllerSignUp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class ControllerSignUp extends GetxController{
+late TextEditingController nom;
+late TextEditingController prenom;
+late TextEditingController age;
+
+
+}
