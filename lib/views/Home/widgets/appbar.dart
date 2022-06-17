878a62fd8987e120e1681814b6344e40d76c23b8
Ray0Emma <ahmadifarah22@gmail.com>
2022-06-17 00:13:20 +0000

    feat: hide/show user profile

--- lib/views/Home/widgets/appbar.dart
+++ lib/views/Home/widgets/appbar.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:roomie/resources/app_colors.dart';
 import '../widgets/searchBar.dart';
@@ -15,13 +16,16 @@ Widget MyAppBar() {
             Container(
               height: 50,
               width: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  // color: Colors.blueAccent,
-                  border: Border.all(color: AppColors.GRAY_COLOR, width: 4),
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/users/user1.jpg"),
-                      fit: BoxFit.cover)),
+              decoration: (FirebaseAuth.instance.currentUser != null)
+                  ? BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      // color: Colors.blueAccent,
+                      border: Border.all(color: AppColors.GRAY_COLOR, width: 4),
+                      image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/users/user1.jpg"), //user profile
+                          fit: BoxFit.cover))
+                  : null,
             ),
             Align(
               alignment: Alignment.center,
