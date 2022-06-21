fe29a54b4724a7f79e5e73db4cedc1113b953ea9
bourigue <rrbourigue@gmail.com>
2022-06-21 18:42:00 +0100

    fix:fixed add date adesse and equipment and gefulation from firstore

--- lib/controllers/Binging.dart
+++ lib/controllers/Binging.dart
@@ -1,6 +1,7 @@
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 
+import 'RoomeDetails.dart';
 import 'commandeController.dart';
 
 
@@ -9,6 +10,8 @@ class Binding implements Bindings{
   @override
   void dependencies() {
     Get.put(comandeContrller(),permanent: true);
+    Get.put(roomeiDetails(),permanent: true);
+
 
   }
 
--- lib/views/roomDetails/room.dart
+++ lib/views/roomDetails/room.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:roomie/models/PostModel.dart';
 import 'package:roomie/resources/app_styles.dart';
 import 'package:roomie/resources/firebase_auth_constants.dart';
 
@@ -13,6 +14,8 @@ import 'package:roomie/views/roomDetails/widget/character.dart';
 import 'package:roomie/views/roomDetails/widget/imagepost.dart';
 import 'package:roomie/views/roomDetails/widget/profileUser.dart';
 
+import '../../controllers/RoomeDetails.dart';
+
 class rommDetails extends StatefulWidget {
   const rommDetails({Key? key}) : super(key: key);
   @override
@@ -20,8 +23,15 @@ class rommDetails extends StatefulWidget {
 }
 
 class _rommDetailsState extends State<rommDetails> {
+  roomeiDetails c = Get.find();
+
   var idPost = Get.arguments;
   List lang = ["france", "merican", "souse", "tamazurt"];
+  @override
+  void initState() {
+    
+    super.initState();
+  }
 
   Widget build(BuildContext context) {
     return SafeArea(
@@ -53,95 +63,110 @@ class _rommDetailsState extends State<rommDetails> {
               ),
             ),
             Expanded(
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(30),
-                          topLeft: Radius.circular(30))),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        imagepost(
-                            "https://firebasestorage.googleapis.com/v0/b/roomie-589a9.appspot.com/o/photos%2F6f92a1948e175f59385308b580ae153c.png?alt=media&token=83111565-9989-464f-a417-1e7ac0d0bab4"),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Column(
-                            children: [
-                              adressepost(
-                                  "N 41 LOTS REDDAD, OUMDHAR, BENI MELLAL",
-                                  3,
-                                  "Beni Mellal"),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              characher("Equipment", lang),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              characher("Regulation", lang),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              profileUser(
-                                  "https://firebasestorage.googleapis.com/v0/b/roomie-589a9.appspot.com/o/photos%2F6f92a1948e175f59385308b580ae153c.png?alt=media&token=83111565-9989-464f-a417-1e7ac0d0bab4",
-                                  "Farah hamada",
-                                  "nice girl helped rachid for learning ",
-                                  "13",
-                                  "profetionnele"),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              characher("Languages", lang),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              characher("Personality", lang),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              characher("LifeStyle", lang),
-                              Divider(
-                                thickness: 3,
-                              ),
-                              characher("Hobbis", lang),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+              child: GetBuilder<roomeiDetails>(
+                builder: (roomeidetails) =>Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            topLeft: Radius.circular(30))),
+                    child: SingleChildScrollView(
+                      child:   FutureBuilder(
+                        future: roomeidetails.getDataUser(idPost),
+                        builder:
+                            (BuildContext context, AsyncSnapshot usnapshotUser) {
+                          return   FutureBuilder(
+                            future: roomeidetails.getDataPost(idPost),
+                            builder:
+                                (BuildContext context, AsyncSnapshot usnapshotpost) {
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text(
-                                    "${100} DH/ month",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w800,
-                                        fontSize: 20),
-                                  ),
-                                  ElevatedButton.icon(
-                                    onPressed: () {},
-                                    icon: Icon(Icons.call),
-                                    label: Text(
-                                      "call",
-                                      style: TextStyle(fontSize: 20),
+                                  imagepost(usnapshotpost.data["imageUri"])
+                                  ,
+                                  Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 16),
+                                    child: Column(
+                                      children: [
+                                        adressepost(
+                                            usnapshotpost.data["addresse"],
+                                            usnapshotpost.data["capacity"],
+                                            citys[usnapshotpost.data["city"]]),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        characher("Equipment", usnapshotpost.data["equipment"]),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        characher("Regulation", usnapshotpost.data["regulation"]),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        profileUser(
+                                            usnapshotUser.data["profile"],
+                                            usnapshotUser.data["name"],
+                                            usnapshotUser.data["about"],
+                                            "13",
+                                            "profetionnele"),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        characher("Languages", lang),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        characher("Personality", lang),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        characher("LifeStyle", lang),
+                                        Divider(
+                                          thickness: 3,
+                                        ),
+                                        characher("Hobbis", lang),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              "${100} DH/ month",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w800,
+                                                  fontSize: 20),
+                                            ),
+                                            ElevatedButton.icon(
+                                              onPressed: () {},
+                                              icon: Icon(Icons.call),
+                                              label: Text(
+                                                "call",
+                                                style: TextStyle(fontSize: 20),
+                                              ),
+                                              style: ElevatedButton.styleFrom(
+                                                  primary: AppColors.PRIMARY_COLOR,
+                                                  padding: EdgeInsets.symmetric(
+                                                      horizontal: 25, vertical: 12)),
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                      ],
                                     ),
-                                    style: ElevatedButton.styleFrom(
-                                        primary: AppColors.PRIMARY_COLOR,
-                                        padding: EdgeInsets.symmetric(
-                                            horizontal: 25, vertical: 12)),
                                   )
                                 ],
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )),
+                              );
+
+                            },
+                          );
+
+                        },
+                      ),
+                    ))) ,
             ),
           ],
         ),
